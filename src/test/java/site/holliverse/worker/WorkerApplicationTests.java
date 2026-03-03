d0b104aec67d82ad8571218dc7f4e0709702d7e2
bon0512 <nonstop00450@gmail.com>
2026-03-03 20:28:20 +0900

    [HSC-178] build: 자코코 설정

---
+++ src/test/java/site/holliverse/worker/WorkerApplicationTests.java
@@ -0,0 +1,12 @@
+package site.holliverse.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
